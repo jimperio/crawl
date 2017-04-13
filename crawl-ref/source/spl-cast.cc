7582b0345d51f9ed21eb1f388a70895a1f20d213
advil <rawlins@gmail.com>
2017-04-13 10:18:05 -0400

    Add scale parameters, for better spell power resolution
    This adds a scale parameter to `stepdown_spellpower` and to
    `calc_spell_power`, and uses the scale parameter to get higher
    resolution in `raw_spell_fail`.  This results in much more continuous
    behavior of spell fail rates when training, though it is still not high
    enough resolution to be perfectly continuous, especially at high skill
    levels.
    
    The behavior of the two changed functions with default arguments remains
    the same.

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -325,7 +325,8 @@ int raw_spell_fail(spell_type spell)
     int chance = 60;
 
     // Don't cap power for failure rate purposes.
-    chance -= 6 * calc_spell_power(spell, false, true, false); // realistic range: -6 to -366 (base, -1 to -61)
+    // scale by 6, which I guess was chosen because it seems to work.
+    chance -= calc_spell_power(spell, false, true, false, 6); // realistic range: -6 to -366 (before scale -1 to -61)
     chance -= (you.intel() * 2); // realistic range: -2 to -70
 
     const int armour_shield_penalty = player_armour_shield_spell_penalty();
@@ -391,13 +392,34 @@ int raw_spell_fail(spell_type spell)
     return min(max(chance2, 0), 100);
 }
 
-int stepdown_spellpower(int power)
+/*
+ * Given some spellpower in centis, do a stepdown at around 50 (5000 in centis) and return a rescaled value.
+ *
+ * @param power the input spellpower in centis.
+ * @param scale a value to scale the result by, between 1 and 1000. Default is 1, which returns a regular spellpower.  1000 gives you millis, 100 centis.
+ */
+int stepdown_spellpower(int power, int scale)
 {
-    return stepdown_value(power / 100, 50, 50, 150, 200);
+    // use millis internally
+    ASSERT_RANGE(scale, 1, 1000);
+    const int divisor = 1000 / scale;
+    int result = stepdown_value(power * 10, 50000, 50000, 150000, 200000) / divisor;
+    return result;
 }
 
+/*
+ * Calculate spell power.
+ *
+ * @param spell         the spell to check
+ * @param apply_intel   whether to include intelligence in the calculation
+ * @param fail_rate_check is this just a plain failure rate check or should it incorporate situational facts and mutations?
+ * @param cap_power     whether to apply the power cap for the spell (from `spell_power_cap(spell)`)
+ * @param scale         what scale to apply to the result internally?  This function has higher internal resolution than the default argument, so use this rather than dividing. This must be between 1 and 1000.
+ *
+ * @return the resulting spell power.
+ */
 int calc_spell_power(spell_type spell, bool apply_intel, bool fail_rate_check,
-                     bool cap_power)
+                     bool cap_power, int scale)
 {
     int power = 0;
 
@@ -448,11 +470,13 @@ int calc_spell_power(spell_type spell, bool apply_intel, bool fail_rate_check,
         power /= 10 + (you.props[HORROR_PENALTY_KEY].get_int() * 3) / 2;
     }
 
-    power = stepdown_spellpower(power);
+    // at this point, `power` is assumed to be basically in centis.
+    // apply a stepdown, and scale.
+    power = stepdown_spellpower(power, scale);
 
     const int cap = spell_power_cap(spell);
     if (cap > 0 && cap_power)
-        power = min(power, cap);
+        power = min(power, cap * scale);
 
     return power;
 }
